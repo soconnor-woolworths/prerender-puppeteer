527a7ae1da351d6d818e58867099110f4302681c
Hiren Gondhiya <hgondhiya@woolworths.com.au>
2022-05-24 22:30:05 +1000

    connect/disconnect to db only once

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
+import { Mongoose } from 'mongoose';
+import { connectMongoDb } from './connect-db';
 import { Scraper } from './scraper';
 import { Uploader } from './uploader';
 
+let mongoose: Mongoose;
+connectMongoDb()
+  .then(() => {
 const scraper = new Scraper(new Uploader());
 scraper.scrap();
+  })
+  .catch((err) => {
+    console.error({ err });
+  })
+  .finally(() => {
+    if (mongoose) {
+      mongoose.disconnect();
+    }
+  });
 
 // TODO: Get list of urls we want to exclude query params from
 // TODO: Scrape mobile, tablet and desktop versions
